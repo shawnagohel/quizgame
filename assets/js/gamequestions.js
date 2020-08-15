68a86e5aae68646e5e6fa321e78875ca06e2b2b7
Shawna Gohel <shawnagohel@Shawnas-MacBook-Air.local>
2020-08-14 20:16:16 -0700

    initial commit

---
+++ assets/js/gamequestions.js
@@ -0,0 +1,42 @@
+// function loadQuestions() {
+  const questions = [
+    {
+        title: "Question 1: Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "Question 2: The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Question 3: Which of the following is an example of a single line comment?",
+        choices: ["// Is this a comment?",
+
+        "console.log()",
+  
+        "console.log('Is this a comment?');",
+  
+        "'Is this a comment?"],
+        answer: "// Is this a comment?"
+    },
+    {
+        title: "Question 4: What is the correct way to declare a new variable that you can change?",
+        choices: ["let myName = 'Sloan'",
+
+        "myName = 'Sloan'",
+  
+        "let myName: 'Sloan'",
+  
+        "const myName = 'Sloan'"],
+        answer: "let myName = 'Sloan'"
+    },
+    {
+        title: "Question 5: The logical operator that represents 'or' is ____.",
+        choices: ["||", "OR", "&&", "==="],
+        answer: "||"
+    }
+];
+// };
+// loadQuestions();
